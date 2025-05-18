39225a77f22f2ff7c381be8d8c3704792116466d
Mohamedkhames1 <moham01555@gmail.com>
2025-05-18 20:45:04 +0300

    Updete count present sheep

--- count_present_sheep.js
+++ count_present_sheep.js
@@ -1,10 +1,12 @@
 function count_present_sheep(sheeps) {
-  let count = 0;
-  if (!sheeps) return count;
-  for (let i = 0; i < sheeps.length; i++) {
-    if (sheeps[i]) ++count;
-  }
-  return count;
+  // let count = 0;
+  // if (!sheeps) return count;
+  // for (let i = 0; i < sheeps.length; i++) {
+  //   if (sheeps[i]) ++count;
+  // }
+  // return count;
+
+  return sheeps.filter((x) => x).length;
 }
 
-console.log(count_present_sheep([true, true, true, false]));
+console.log(count_present_sheep([false, true, true, true, false]));
