ccb11417c123042f7254d0b4062dc9c021e08932
Mohamedkhames1 <moham01555@gmail.com>
2025-05-18 15:22:31 +0300

    count present sheep

---
+++ count_present_sheep.js
@@ -0,0 +1,10 @@
+function count_present_sheep(sheeps) {
+  let count = 0;
+  if (!sheeps) return count;
+  for (let i = 0; i < sheeps.length; i++) {
+    if (sheeps[i]) ++count;
+  }
+  return count;
+}
+
+console.log(count_present_sheep([true, true, true, false]));
